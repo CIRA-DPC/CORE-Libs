29f104f2f511e7506ce49adc3f5e06ad30cb9edf
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2024-04-23 05:47:44 +0000

    Update to work for gnu

--- build_package.sh
+++ build_package.sh
@@ -10,33 +10,39 @@ realpath() {
 if [ -z ${PREFIX} ]; then
     echo "Environment variable 'PREFIX' must be set"
 fi
-if [ -z ${ONEAPI_PATH} ]; then
-    ONEAPI_PATH=/opt/intel/oneapi
-fi
+# if [ -z ${ONEAPI_PATH} ]; then
+#     ONEAPI_PATH=/opt/intel/oneapi
+# fi
 
 SRCFILE=$(realpath $1)
 TEMPDIR=${PWD}/tmp
 
 if [ -d "${TEMPDIR}" ]; then
-    rmdir ${TEMPDIR}
+    rm -rf ${TEMPDIR}
 fi
 mkdir -p ${TEMPDIR}
 
-set +e
-source ${ONEAPI_PATH}/setvars.sh
-sv_ret=$?
-set -e
-if [[ "${sv_ret}" == "3" ]]; then
-    echo "Intel environment already set"
-elif [[ "${sv_ret}" != "0" ]]; then
-    echo "Error encountered in /opt/intel/oneapi/setvars.sh"
-    exit ${sv_ret}
-fi
+# set +e
+# source ${ONEAPI_PATH}/setvars.sh
+# sv_ret=$?
+# set -e
+# if [[ "${sv_ret}" == "3" ]]; then
+#     echo "Intel environment already set"
+# elif [[ "${sv_ret}" != "0" ]]; then
+#     echo "Error encountered in /opt/intel/oneapi/setvars.sh"
+#     exit ${sv_ret}
+# fi
 
 echo $?
 
 cd ${TEMPDIR}
 tar -xzvf "${SRCFILE}" --directory "${TEMPDIR}" --strip-components=1
+
+echo "**********************************************************"
+echo LIBS: ${LIBS}
+echo PATH: ${PATH}
+echo LD_LIBRARY_PATH: ${LD_LIBRARY_PATH}
+echo "**********************************************************"
 
 ./configure --prefix=$(realpath ${PREFIX}) ${CONFIGFLAGS}
 make
